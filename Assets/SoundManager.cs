9b4ff5cc13d241c534e538055e4e7ed01b3a7cfd
Rahadian Rayhan <rahadian.rayhan@gmail.com>
2021-03-18 12:13:30 +0700

    Sound Manager
    ini sound pake yg ini

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.audioSource = gameObject.AddComponent<AudioSource>();
+            s.audioSource.clip = s.clip;
+
+            s.audioSource.volume = s.volume;
+            s.audioSource.pitch = s.pitch;
+            s.audioSource.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("MainTheme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sounds => sounds.name == name);
+        s.audioSource.Play();
+    }
+}
