f30c55451269d4356e6c1946ecf8566b2ce03a32
Rahmat Maulana <rahmatmpr@rocketmail,com>
2021-03-18 19:44:19 +0700

    Add Game Manager
    Game Manager and FadeIn or FadeOut

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    [SerializeField] Animator animFade;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        FadeScreen("FadeIn");
+    }
+
+    //call me for fade the screen
+    public void FadeScreen(string state)
+    {
+        animFade.Play(state);
+    }
+}
