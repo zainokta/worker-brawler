bb79b9e5015584da1ebe915404ae4835ad695ec0
Rahadian Rayhan <rahadian.rayhan@gmail.com>
2021-03-19 19:47:18 +0700

    Login Fix
    Final login

--- Assets/Scripts/Login/Login.cs
+++ Assets/Scripts/Login/Login.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using Photon.Pun;
-using Photon.Realtime;
 
 public class Login : MonoBehaviour
 {
+    [Header("UI Panel")]
     [SerializeField] GameObject UserNameMenu;
     [SerializeField] GameObject HelloMenu;
+    [SerializeField] GameObject FalseMenu;
+
+    [Header("Username")]
     [SerializeField] private InputField UsernameInput;
+
+    [Header("Text")]
     [SerializeField] Text OpenHello;
 
     // Start is called before the first frame update
@@ -20,13 +26,35 @@ public class Login : MonoBehaviour
 
     public void SetUsername()
     {
-        UserNameMenu.SetActive(false);
-        PhotonNetwork.NickName = UsernameInput.text;
-        OpenFuncHello();
+        if(UsernameInput.text.Length > 3)
+        {
+            UserNameMenu.SetActive(false);
+            PhotonNetwork.NickName = UsernameInput.text;
+            OpenFuncHello();
+        }
+        if(UsernameInput.text.Length <= 3)
+        {
+            FalseMenu.SetActive(true);
+            StartCoroutine(FalseTextUI());
+        }
     }
     void OpenFuncHello()
     {
+        FalseMenu.SetActive(false);
         HelloMenu.SetActive(true);
         OpenHello.text = "Welcome : " + PhotonNetwork.NickName;
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    IEnumerator FalseTextUI()
+    {
+        yield return new WaitForSeconds(5);
+        FalseMenu.SetActive(false);
     }
 }
