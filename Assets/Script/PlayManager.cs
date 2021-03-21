28bacd6bcebf01b038242f2a9d51f3b9656e845f
zlaxen <zeronitezero@gmail.com>
2021-03-21 18:30:51 +0700

    Message Commit

--- Assets/Script/PlayManager.cs
+++ Assets/Script/PlayManager.cs
@@ -5,22 +5,55 @@ using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class PlayManager : MonoBehaviour
+public class PlayManager : MonoBehaviourPunCallbacks
 {
     public GameObject PlayerPref;
     //public GameObject GameCanvas;
     public GameObject SceneCamera;
     public Text PingText;
+
+    public static float GameTime = 180f;
+    public Text TimeInGame;
+
+    public Text LoseWinState;
+
     // Start is called before the first frame update
     private void Awake()
     {
+        TimeInGame.gameObject.SetActive(false);
         //GameCanvas.SetActive(true);
         SpawnPlayer();
     }
 
     private void Update()
     {
-        PingText.text = "Ping : " + PhotonNetwork.GetPing();
+        PingText.text = "Ping : " + PhotonNetwork.GetPing() + " fps";
+
+        if(PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        {
+            TimeInGame.gameObject.SetActive(true);
+            GameTime -= 0.01f;
+            TimeInGame.text = GameTime.ToString("0");
+        }
+        else
+        {
+            TimeInGame.gameObject.SetActive(false);
+        }
+
+        if (Health.YouLose == true)
+        {
+            LoseWinState.gameObject.SetActive(true);
+            LoseWinState.text = "You Lose"; 
+        }
+        else if(Health.YouWin == true)
+        {
+            LoseWinState.gameObject.SetActive(true);
+            LoseWinState.text = "You Win";
+        }
+
+        Debug.Log(Health.YouLose);
+        Debug.Log(Health.YouWin);
+        Debug.Log(GameTime);
     }
     public void SpawnPlayer()
     {
